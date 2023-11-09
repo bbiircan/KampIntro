3db039c15ee0f6146ea09f7431a483b983c103f8
bbiircan <bircanbircan1963@gmail.com>
2023-11-09 10:07:18 +0300

    8 Kasım Ödev1

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Generic;
+
+//string[] isimler = new string[] { "Rümeysa", "Bircan" };
+//Console.WriteLine(isimler[0]);
+//Console.WriteLine(isimler[1]);
+
+//isimler = new string[3];
+//isimler[2] = "Eylül Ada";
+//Console.WriteLine(isimler[2]);
+//Console.WriteLine(isimler[0]);
+
+List<string> isimler2= new List<string> {"Rümeysa","Bircan"};
+Console.WriteLine(isimler2[0]);
+Console.WriteLine(isimler2[1]);
+isimler2.Add("Eylül Ada");
+Console.WriteLine(isimler2[2]);
+Console.WriteLine(isimler2[0]);
